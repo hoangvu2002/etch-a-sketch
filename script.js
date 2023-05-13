f8649f62a6d80efcfea5dcea9077b95495e28da2
pdhoangvu <pdhoangvu@gmail.com>
2023-05-13 21:04:51 +0700

    Sketch out how I want to lay out my divs using DOM

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.createElement("div");
+const 
+
+// this is just a draft of what i will do
+// first i will create a container div using DOM
+// i wil create 16 more divs ( each div with flex-direction: row) for each row
+// append lay out 16 divs in the container div using flex-direction: column
+// in each row div, create 16 more divs with the lay out row div of display: flex; flex-direction: row;
+//remember to use box-sizing: border-box for each div for ease of use
