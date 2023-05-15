4ac68730d89f776014970188b60ffdc549eeee1f
pdhoangvu <pdhoangvu@gmail.com>
2023-05-16 03:15:05 +0700

    Change the approach, not adding row div anymore, just add divs directly, seems like i got the hang of grid now

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ body.appendChild(gridContainer);
 
 function createGrid(rows, columns) {
   //gridContainer=document.getElementById("grid-container");
-  gridContainer.innerHTML = ''; // Clear previous grid
+  //gridContainer.innerHTML = ''; // Clear previous grid
   gridContainer.style.display = 'grid';
   gridContainer.style.width = '960px';
   gridContainer.style.height = '960px';
@@ -15,17 +15,19 @@ function createGrid(rows, columns) {
   gridContainer.style.gridTemplateColumns = `repeat(${columns}, 1fr)`; // Set the number of columns
   gridContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`; // Set the number of rows
 
-  for (let i = 0; i < rows; i++) {
+  for (let i = 0; i < rows*columns; i++) {
     const rowDiv = document.createElement('div');   
     rowDiv.classList.add('grid-row');
+    rowDiv.style.display = "flex";
 
-    for (let j = 0; j < columns; j++) {
-      const cellDiv = document.createElement('div');
-      cellDiv.classList.add('grid-cell');
-      rowDiv.appendChild(cellDiv);
-    }
-
+//    for (let j = 0; j < columns; j++) {
+//      const cellDiv = document.createElement('div');
+//      cellDiv.classList.add('grid-cell');
+//      rowDiv.appendChild(cellDiv);
+//    }
+//
     gridContainer.appendChild(rowDiv);
+
   }
   
   
